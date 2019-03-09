57de9a6e09473f643d192560651e31638648cc0c
Thomas Crampton <celtichammer01@gmail.com>
2019-03-09 16:07:33 -0600

    Latest

--- LSTM_UML/Nodes/AbstractNode.cs
+++ LSTM_UML/Nodes/AbstractNode.cs
@@ -1,1 +1,13 @@
+using System.Collections.Generic;
 
+namespace LSTM_UML.Nodes
+{
+    public abstract class AbstractNode
+    {
+        public abstract float Val { get; set; }
+        public abstract List<Connector> InboundConnectors { get; set; }
+        public abstract List<Connector> OutboundConnectors { get; set; }
+
+        protected abstract void makeListOfConnectors();
+    }
+}
